0a7142c91eb39d713849a99dddc1a34c3509be2c
Kristi Achilleos <Krispy2009@gmail.com>
2021-11-17 15:58:10 +0200

    add create message DTO and wconnect validation pipe

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDTO } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -9,14 +10,12 @@ listMessages() {
 }
 
 @Post()
-
-createMessage(@Body() body: any) {
+createMessage(@Body() body: CreateMessageDTO) {
     console.log(body)
     return `Will save ${body.content} to DB`
 }
 
 @Get('/:id')
-
 getMessage(@Param('id') id: string) {
     return `Return one message with id ${id}`
 }
