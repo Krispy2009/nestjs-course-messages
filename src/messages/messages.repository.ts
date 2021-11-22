aadfe416897be647596e2f3a1261861a9a9e1a5f
Kristi Achilleos <Krispy2009@gmail.com>
2021-11-22 11:36:21 +0200

    add error control

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -5,7 +5,7 @@ export class MessagesRepository {
         const contents = await readFile('messages.json', 'utf8')
         const messages = JSON.parse(contents)
         
-        return messages[id] || 'NOOOP'
+        return messages[id]
 
     }
     async findAll() {
