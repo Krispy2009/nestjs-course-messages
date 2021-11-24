5f39d4c1ae14c773808e9ff3d023af7c94d43a73
Kristi Achilleos <Krispy2009@gmail.com>
2021-11-24 11:35:30 +0200

    refactor for DI

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,5 +1,8 @@
+
+import { Injectable } from "@nestjs/common";
 import { MessagesRepository } from "./messages.repository";
 
+@Injectable()
 export class MessagesService {
 
     constructor(public messagesRepo: MessagesRepository) {}
